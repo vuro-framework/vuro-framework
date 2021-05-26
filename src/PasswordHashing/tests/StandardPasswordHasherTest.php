9ebe89305bb0cc719ecf2c6b599be5502754f2b8
Ashley <38263557+ularanigu@users.noreply.github.com>
2021-05-26 15:12:53 -0400

    Use right copyright name.

--- src/PasswordHashing/tests/StandardPasswordHasherTest.php
+++ src/PasswordHashing/tests/StandardPasswordHasherTest.php
@@ -2,7 +2,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2021 LovePHPForever
+ * Copyright (c) 2021 Vuro Framework
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
